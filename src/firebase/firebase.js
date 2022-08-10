bd6c2e4c6ffe2e4f2a8cc06c928e6c8f4f74ff70
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-08-11 00:31:28 +0900

    firebaseの設定

---
+++ src/firebase/firebase.js
@@ -0,0 +1,8 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "../env";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
