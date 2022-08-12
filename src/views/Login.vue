b16d23a401d4069c16737a7ce913cf941e200e08
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-08-13 00:28:17 +0900

    ログインフォームの作成

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,13 +4,8 @@ import { reactive } from "vue";
 // do not use same name with ref
 const form = reactive({
   name: "",
-  region: "",
-  date1: "",
-  date2: "",
-  delivery: false,
-  type: [],
-  resource: "",
-  desc: "",
+  mail: "",
+  password: "",
 });
 
 const onSubmit = () => {
@@ -27,67 +22,32 @@ export default {
 <template>
   <div class="container">
     <el-container>
-      <el-header>Header</el-header>
-      <el-main
-        ><el-form :model="form" label-width="120px">
-          <el-form-item label="Activity name">
-            <el-input v-model="form.name" />
-          </el-form-item>
-          <el-form-item label="Activity zone">
-            <el-select
-              v-model="form.region"
-              placeholder="please select your zone"
-            >
-              <el-option label="Zone one" value="shanghai" />
-              <el-option label="Zone two" value="beijing" />
-            </el-select>
-          </el-form-item>
-          <el-form-item label="Activity time">
-            <el-col :span="11">
-              <el-date-picker
-                v-model="form.date1"
-                type="date"
-                placeholder="Pick a date"
-                style="width: 100%"
-              />
-            </el-col>
-            <el-col :span="2" class="text-center">
-              <span class="text-gray-500">-</span>
-            </el-col>
-            <el-col :span="11">
-              <el-time-picker
-                v-model="form.date2"
-                placeholder="Pick a time"
-                style="width: 100%"
-              />
-            </el-col>
-          </el-form-item>
-          <el-form-item label="Instant delivery">
-            <el-switch v-model="form.delivery" />
-          </el-form-item>
-          <el-form-item label="Activity type">
-            <el-checkbox-group v-model="form.type">
-              <el-checkbox label="Online activities" name="type" />
-              <el-checkbox label="Promotion activities" name="type" />
-              <el-checkbox label="Offline activities" name="type" />
-              <el-checkbox label="Simple brand exposure" name="type" />
-            </el-checkbox-group>
-          </el-form-item>
-          <el-form-item label="Resources">
-            <el-radio-group v-model="form.resource">
-              <el-radio label="Sponsor" />
-              <el-radio label="Venue" />
-            </el-radio-group>
-          </el-form-item>
-          <el-form-item label="Activity form">
-            <el-input v-model="form.desc" type="textarea" />
-          </el-form-item>
-          <el-form-item>
-            <el-button type="primary" @click="onSubmit">Create</el-button>
-            <el-button>Cancel</el-button>
-          </el-form-item>
-        </el-form></el-main
-      >
+      <el-header>
+        <el-menu mode="horizontal" :ellipsis="false">
+          <router-link to="/">
+            <el-menu-item>LOGO</el-menu-item>
+          </router-link>
+        </el-menu>
+      </el-header>
+      <el-main>
+        <el-card class="box-card" shadow="hover">
+          <el-form :model="form" label-width="120px">
+            <el-form-item label="Name">
+              <el-input v-model="form.name" />
+            </el-form-item>
+            <el-form-item label="E-mail">
+              <el-input v-model="form.mail" />
+            </el-form-item>
+            <el-form-item label="Password">
+              <el-input v-model="form.password" />
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="onSubmit">Create</el-button>
+              <el-button>Cancel</el-button>
+            </el-form-item>
+          </el-form>
+        </el-card>
+      </el-main>
     </el-container>
   </div>
 </template>
