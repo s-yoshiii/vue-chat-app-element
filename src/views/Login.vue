b366193871ef980ef61180f8f1edbb1d8fda0050
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-08-15 00:26:04 +0900

    インプットフォームのバリデーション

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { computed, reactive } from "vue";
+import { object, string } from "yup";
 // do not use same name with ref
 const form = reactive({
   name: "",
@@ -7,10 +8,45 @@ const form = reactive({
   password: "",
 });
 
+const errorMsg = reactive({
+  name: "",
+  mail: "",
+  password: "",
+});
+
 // バリデーション関数
+const emailRegExp = /.+@.+\..+/;
+
+// 入力スキーマ
+const inputSchema = object().shape({
+  name: string().trim().required("名前を入力して下さい"),
+  mail: string()
+    .trim()
+    .required("メールアドレスを入力してください。")
+    .matches(emailRegExp, {
+      message: "正しい形式のメールアドレスを入力してください。",
+    }),
+  password: string()
+    .trim()
+    .trim()
+    .min(10, "10文字以上のパスワードを入力してください。"),
+});
+
+// バリデーション処理
+const valid = (field) => {
+  inputSchema
+    .validateAt(field, form)
+    .then(() => {
+      errorMsg[field] = "";
+    })
+    .catch((err) => {
+      errorMsg[field] = err.message;
+    });
+};
+
 const isValid = computed(() => {
   console.log("isValid", isValid.value);
-  return !isValid.value;
+  return isValid.value;
 });
 
 // submit
@@ -40,14 +76,31 @@ export default {
           <h1 class="form-title">Sing Up</h1>
           <el-form :model="form" label-width="120px">
             <el-form-item label="Name">
-              <el-input v-model="form.name" />
-              <p>{{ nameError }}</p>
+              <el-input v-model="form.name" @blur="valid('name')" />
+              <p v-if="!!errorMsg.name">
+                {{ errorMsg.name }}
+              </p>
             </el-form-item>
             <el-form-item label="E-mail">
-              <el-input v-model="form.mail" type="email" />
+              <el-input
+                v-model="form.mail"
+                type="email"
+                @blur="valid('mail')"
+              />
+              <p v-if="!!errorMsg.mail">
+                {{ errorMsg.mail }}
+              </p>
             </el-form-item>
             <el-form-item label="Password">
-              <el-input v-model="form.password" type="password" />
+              <el-input
+                v-model="form.password"
+                type="password"
+                @blur="valid('password')"
+              />
+
+              <p v-if="!!errorMsg.password">
+                {{ errorMsg.password }}
+              </p>
             </el-form-item>
             <el-form-item>
               <el-button type="primary" @click="onSubmit" :disabled="isValid"
